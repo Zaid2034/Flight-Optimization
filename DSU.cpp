5acf3b4830a2452f5ba8a2291e290849e46846a0
Zaid2034 <mdzaidali2034@gmail.com>
2024-02-24 01:11:30 +0530

    algorithms

---
+++ DSU.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int u;
+    int v;
+    int wt;
+    node(int first, int second, int weight)
+    {
+        u = first;
+        v = second;
+        wt = weight;
+    }
+};
+
+bool comp(node a, node b)
+{
+    return a.wt < b.wt;
+}
+
+int findPar(int u, vector<int> &parent)
+{
+    if (u == parent[u])
+        return u;
+    return parent[u] = findPar(parent[u], parent);
+}
+
+void unionn(int u, int v, vector<int> &parent, vector<int> &rank)
+{
+    u = findPar(u, parent);
+    v = findPar(v, parent);
+    if (rank[u] < rank[v])
+    {
+        parent[u] = v;
+    }
+    else if (rank[v] < rank[u])
+    {
+        parent[v] = u;
+    }
+    else
+    {
+        parent[v] = u;
+        rank[u]++;
+    }
+}
